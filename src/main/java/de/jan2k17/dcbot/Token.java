baf31ba0c46e4d6265110e126e98f2462710b8aa
Jan2k17 <56897434+Jan2k17@users.noreply.github.com>
2023-08-23 23:23:23 +0200

    Update Token.java
    removed token

--- src/main/java/de/jan2k17/dcbot/Token.java
+++ src/main/java/de/jan2k17/dcbot/Token.java
@@ -1,5 +1,5 @@
 package de.jan2k17.dcbot;
 
 public class Token {
-    public static String token = "MTA0MjczMTY1OTEwOTM1MTQ1NA.GbPC1h.Ez4ZC_STQxFsC8rU4MTcXKzBRVhQ_BeimFPf4o";
+    public static String token = "";
 }
