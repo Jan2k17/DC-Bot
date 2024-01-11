1053f4fd0c856a841b2485a09c049e544251b853
Jan2k17 <56897434+Jan2k17@users.noreply.github.com>
2024-01-11 22:46:18 +0100

    Update Token.java

--- src/main/java/de/jan2k17/dcbot/Token.java
+++ src/main/java/de/jan2k17/dcbot/Token.java
@@ -2,5 +2,5 @@ package de.jan2k17.dcbot;
 
 public class Token {
     // DC-Bot: Jan2k17_BOT
-    public static String token = "MTE5NDA5ODY3Nzk4NDI4NDc1Mw.GfwVs9.mqsHSfK7FnsAdo1iDwc-eqA9yKvtFOp7ox7B3o";
+    public static String token = "";
 }
